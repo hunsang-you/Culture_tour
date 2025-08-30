77f08c21d77d347dbb04b8253070af21e698c0fb
hunsang <gjstkd123@naver.com>
2025-08-30 15:02:48 +0900

    url 수정

--- src/app/api/festival/route.ts
+++ src/app/api/festival/route.ts
@@ -9,7 +9,7 @@ export async function GET(req: Request) {
   const API_URL =
     process.env.NODE_ENV === "development"
       ? process.env.FESTIVAL_API_URL!
-      : "https://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api";
+      : "api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api";
 
   const url = new URL(API_URL);
   url.searchParams.append("ServiceKey", SERVICE_KEY);
